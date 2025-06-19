ce493a237069d047593524a05e6c3ed5d9b1910f
Carlos Eduardo <kadumaroso@gmail.com>
2025-06-19 09:05:57 -0300

    Method findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,10 +14,15 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
 		Seller seller = sellerDao.findById(3);
+//		System.out.println(seller);
 		
-		System.out.println(seller);
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
